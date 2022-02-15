f354a43add5112645f5b8233bc2dce8a1243f979
ACSD1710 <96124322+ACSD1710@users.noreply.github.com>
2022-02-15 08:57:15 +0100

    Update homework_HINTS.js
    Changes

--- homework_HINTS.js
+++ homework_HINTS.js
@@ -1,55 +1,41 @@
-
-
-
-
 async function students() {
     try{
         let result = await fetch("https://raw.githubusercontent.com/sedc-codecademy/skwd9-04-ajs/main/Samples/students_v2.json")
-        console.log(result);
         let students = await result.json();
         console.log(students);
 
         /////1/////
 
-        let averadgeHigherThan3 = students.filter( (student) => {
-         return  student.averageGrade > 3} )
-         console.log(averadgeHigherThan3);
+        let averadgeHigherThan3 = students.filter( (student) => student.averageGrade > 3 )
+        //  console.log(averadgeHigherThan3);
 
         /////2////
 
-        let female = students.filter((student) => { 
-            if(student.gender == "Female" && student.averageGrade ==5)
-            return student });
-        console.log(female);
+        let female = students.filter((student) => student.gender == "Female" && student.averageGrade == 5);
+            
+        // console.log(female);
         
         /////3///
 
-        let male = students.filter((student) => {
-            if (student.city == "Skopje" && student.age > 18) {
-                return student;
-            }
-        }).map((student) =>{ return `${student.firstName} ${student.lastName} `} )
+        let male = students.filter((student) => (student.city == "Skopje" && student.age > 18)              
+            
+        ).map((student) =>`First Name: ${student.firstName} Last Name: ${student.lastName} Age: ${student.age} `);
         
-        console.log(male);
+        // console.log(male);
 
         /////////////////4/////////////////////
 
 
-        let averadgeFemale = students.filter((student) => { 
-            if(student.gender == "Female" && student.age > 24)
-            return student }).map(student =>
-            `${student.firstName} ${student.lastName}, Averadge: ${student.averageGrade}`);
+        let averadgeFemale = students.filter((student) => student.gender == "Female" && student.age > 24)
+            .map(student =>  `${student.firstName} ${student.lastName}, Averadge: ${student.averageGrade}`);
 
-            console.log(averadgeFemale);
+            // console.log(averadgeFemale);
 
        /////////////////////////5//////////////////////
 
 
-        let maleB =students.filter( (student) => {
-             if(student.averageGrade > 2 && student.firstName.charAt(0) == "B") {
-                 return student
-             }  } );
-            console.log(maleB)     
+        let maleB =students.filter( (student) => student.averageGrade > 2 && student.firstName.charAt(0) == "B");
+        console.log(maleB);     
         
         
     }  
